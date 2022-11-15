c7e753bdb0ec0b18ed2ec367efba149356cbd953
nari.yoon <nari.yoon@lge.com>
2022-11-15 10:51:32 +0900

    jbuild error fix

--- app/src/main/java/com/nryoon/mapexample/GoogleMapActivity.kt
+++ app/src/main/java/com/nryoon/mapexample/GoogleMapActivity.kt
@@ -2,14 +2,11 @@ package com.nryoon.mapexample
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.naver.maps.map.MapView
 
-class NaverMapActivity : AppCompatActivity() {
-    private lateinit var mapView: MapView
-    
+class GoogleMapActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_naver_map)
+//        setContentView(R.layout.activity_naver_map)
 
     }
 }
--- app/src/main/java/com/nryoon/mapexample/MainActivity.kt
+++ app/src/main/java/com/nryoon/mapexample/MainActivity.kt
@@ -1,11 +1,33 @@
 package com.nryoon.mapexample
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.googleBtn).setOnClickListener(this::clickListener)
+        findViewById<Button>(R.id.kakaoBtn).setOnClickListener(this::clickListener)
+        findViewById<Button>(R.id.naverBtn).setOnClickListener(this::clickListener)
     }
+
+
+    fun clickListener(view: View) {
+        val targetActivity =
+            when (view.id) {
+                R.id.googleBtn -> GoogleMapActivity::class.java
+                R.id.kakaoBtn -> KakaoMapActivity::class.java
+                R.id.naverBtn -> NaverMapActivity::class.java
+                else -> {
+                    NaverMapActivity::class.java
+                }
+            }
+        startActivity(Intent(this, targetActivity))
+    }
+
 }
